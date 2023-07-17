f4ffbe25b9b0b475ad396d18daf2e8709d02d6c7
charles-42 <71760090+charles-42@users.noreply.github.com>
2023-07-17 15:33:45 +0200

    feat: simple app

---
+++ real_estate_app/main_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import numpy as np
+import pandas as pd
+import pickle
+
+model = pickle.load(open('main_app/static/models/finalized_model.pkl', 'rb'))
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def predict(request):
+    if request.method == 'POST':
+        X_predict = {}
+        for var in ['Year_Built', 'Total_Bsmt_SF', '1st_Flr_SF', 'Gr_Liv_Area','Garage_Area', 'Overall_Qual', 'Full_Bath', 'Exter_Qual',
+                  'Kitchen_Qual', 'Neighborhood']:
+            if var in ['Exter_Qual','Kitchen_Qual', 'Neighborhood']:
+                X_predict[var]= request.POST.get(var)
+            else:
+                X_predict[var]= int(request.POST.get(var))
+
+        pred = model.predict(pd.DataFrame(X_predict, index=[0]))
+
+        return render(request, 'index.html', {'data': int(pred)})
+    else:
+        return HttpResponse("Method Not Allowed")
