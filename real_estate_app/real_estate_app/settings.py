6c121f56a2b8a12056af62831229330fc6599e0c
charles-42 <71760090+charles-42@users.noreply.github.com>
2023-07-18 16:17:08 +0200

    fix(app): :bug: fix coverage

--- real_estate_app/real_estate_app/settings.py
+++ real_estate_app/real_estate_app/settings.py
@@ -13,16 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-# Import coverage package
-import coverage
-
-# Initialize coverage
-cov = coverage.Coverage()
-
-# Start coverage before Django imports your settings
-cov.start()
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
